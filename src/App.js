4b182ec8f310ceca8f147c40fe7334d50eb5f062
Stefano <tepo@hotmail.it>
2023-03-31 15:10:26 +0200

    add env variable

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { FaSearch } from 'react-icons/fa'
 import Photo from './Photo'
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
 
 // 6_Tfpx-3X46CASjkTujMjHw097T4YH-g8uQR9XODBO0
 // _A1aX7nP0HA-oNP8ktMLi2VBiLCqvi5Spb-AyQ3rrYo
 const mainUrl = `https://api.unsplash.com/photos/`
 const searchUrl = `https://api.unsplash.com/search/photos/`
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
 
 function App() {
   const [loading, setLoading] = useState(false);
@@ -14,7 +14,7 @@ function App() {
 
   const fetchImages = async () => {
     setLoading(true);
-    let url = `${mainUrl}?client_id=6_Tfpx-3X46CASjkTujMjHw097T4YH-g8uQR9XODBO0`;
+    let url = `${mainUrl}${clientID}`;
     try {
       const response = await fetch(url);
       const data = await response.json();
